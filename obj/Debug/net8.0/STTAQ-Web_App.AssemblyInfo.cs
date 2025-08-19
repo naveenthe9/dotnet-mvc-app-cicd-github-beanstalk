64e7ab99068906ae10b621c44089ef2af07e0058
naveen <naveen.bodhula@outlook.com>
2025-08-19 15:50:46 +0530

    first commit

--- obj/Debug/net8.0/STTAQ-Web_App.AssemblyInfo.cs
+++ obj/Debug/net8.0/STTAQ-Web_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STTAQ-Web_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd366462b958dc202f973f32da710471e802f536")]
 [assembly: System.Reflection.AssemblyProductAttribute("STTAQ-Web_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STTAQ-Web_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
